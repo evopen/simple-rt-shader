363a60cca32e27097b552ccb6e35103d9a905867
evopen <520dhh@gmail.com>
2021-06-08 20:14:37 +0800

    use patched spirv-std for now

--- simple-rt-shader/build.rs
+++ simple-rt-shader/build.rs
@@ -2,6 +2,7 @@ fn main() {
     spirv_builder::SpirvBuilder::new("../shader", "spirv-unknown-vulkan1.2")
         .capability(spirv_builder::Capability::RayTracingKHR)
         .capability(spirv_builder::Capability::ImageQuery)
+        .capability(spirv_builder::Capability::StorageImageWriteWithoutFormat)
         .extension("SPV_KHR_ray_tracing")
         .build()
         .unwrap();
