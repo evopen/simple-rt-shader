eb9fe6efafc984afefef27587c3b2ac248de787b
evopen <520dhh@gmail.com>
2021-06-10 18:29:06 +0800

    don't need wrapper crate
    because we have shader hot reload!

--- simple-rt-shader/build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    spirv_builder::SpirvBuilder::new("../shader", "spirv-unknown-vulkan1.2")
-        .capability(spirv_builder::Capability::RayTracingKHR)
-        .capability(spirv_builder::Capability::ImageQuery)
-        .capability(spirv_builder::Capability::StorageImageWriteWithoutFormat)
-        .extension("SPV_KHR_ray_tracing")
-        .build()
-        .unwrap();
-}
--- simple-rt-shader/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
-
-pub const SPIRV: &[u8] = include_bytes!(env!("shader.spv"));
